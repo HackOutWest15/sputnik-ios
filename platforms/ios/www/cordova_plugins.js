fce1e703b0129f964df8d9bf2f06feeec3cd8122
Eyvind Niklasson <eyvind.niklasson@gmail.com>
2015-08-11 11:23:16 +0200

    Added all the node files

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,3 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [{"file":"plugins/cordova-plugin-device/www/device.js","id":"cordova-plugin-device.device","clobbers":["device"]}]
+});
