0a6dec5ffafb40ade10dad61f00b15db9fa8279f
Axel Thomson Ek <axel@coursemunk.com>
2015-08-11 17:01:55 +0200

    added dropbox

--- README.md
+++
@@ -1,3 +0,0 @@
-An app to discover new music as you ride along in your train! :)
-
-We like trains.
--- platforms/ios/Sputnik/Classes/MainViewController.h
+++ platforms/ios/Sputnik/Classes/MainViewController.h
@@ -19,7 +19,7 @@
 
 //
 //  MainViewController.h
-//  Sputnik
+//  HelloCordova
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
